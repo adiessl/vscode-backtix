454abf66b20c8a2d11cd7c4f6279db48dac421b4
Alexander Dießl <42088160+adiessl@users.noreply.github.com>
2018-08-15 23:02:56 +0200

    Add string utils

---
+++ src/core/utils/string.utils.ts
@@ -0,0 +1,23 @@
+export function replaceQuoteChars(text: string, targetQuoteChar: string) {
+  const currentQuoteChar = text[0];
+
+  if (currentQuoteChar === targetQuoteChar) {
+    return text;
+  }
+
+  const innerText = text.substring(currentQuoteChar.length, text.length - currentQuoteChar.length);
+
+  const replacedAndEscaped = innerText
+    .replace(new RegExp(`\\\\${currentQuoteChar}`, 'g'), currentQuoteChar)
+    .replace(new RegExp(`${targetQuoteChar}`, 'g'), `\\${targetQuoteChar}`);
+
+  return `${targetQuoteChar}${replacedAndEscaped}${targetQuoteChar}`;
+}
+
+export function replaceLineBreaks(text: string, replaceValue: string = ' ') {
+  return text.replace(/(?:\r\n|\r|\n)/g, replaceValue);
+}
+
+export function wrapText(text: string, wrapper: string): string {
+  return `${wrapper}${text}${wrapper}`;
+}
---
+++ src/test/utils/string.utils.test.ts
@@ -0,0 +1,250 @@
+import * as assert from 'assert';
+import { replaceQuoteChars, replaceLineBreaks, wrapText } from '../../core/utils/string.utils';
+
+suite('string.utils.test', () => {
+  suite('replaceQuoteChars', () => {
+    suite('target: single quotes', () => {
+      suite('simple strings: the quotes should be replaced', () => {
+        test('from single quotes', () => {
+          const original = `'This is a test.'`;
+          const expected = `'This is a test.'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from double quotes', () => {
+          const original = `"This is a test."`;
+          const expected = `'This is a test.'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks', () => {
+          const original = '`This is a test.`';
+          const expected = `'This is a test.'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including single quotes: single quotes should be escaped', () => {
+        test('from single quotes', () => {
+          const original = `"This is a 'test'."`;
+          const expected = `'This is a \\'test\\'.'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks', () => {
+          const original = '`This is a \'test\'.`';
+          const expected = `'This is a \\'test\\'.'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including escaped quotes: should not be escaped afterwards', () => {
+        test('from double quotes, including escaped double quotes', () => {
+          const original = `"This is a \\"test\\"."`;
+          const expected = `'This is a "test".'`;
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks, including escaped backticks', () => {
+          const original = '`This is a \\`test\\`.`';
+          const expected = '\'This is a `test`.\'';
+
+          const actual = replaceQuoteChars(original, '\'');
+
+          assert.equal(actual, expected);
+        });
+      });
+    });
+
+    suite('target: double quotes', () => {
+      suite('simple strings: the quotes should be replaced', () => {
+        test('from single quotes', () => {
+          const original = `'This is a test.'`;
+          const expected = `"This is a test."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from double quotes', () => {
+          const original = `"This is a test."`;
+          const expected = `"This is a test."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks', () => {
+          const original = '`This is a test.`';
+          const expected = `"This is a test."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including double quotes: double quotes should be escaped', () => {
+        test('from single quotes', () => {
+          const original = `'This is a "test".'`;
+          const expected = `"This is a \\"test\\"."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks', () => {
+          const original = '`This is a "test".`';
+          const expected = `"This is a \\"test\\"."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including escaped quotes: should not be escaped afterwards', () => {
+        test('from single quotes, including escaped single quotes', () => {
+          const original = `'This is a \\'test\\'.'`;
+          const expected = `"This is a 'test'."`;
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks, including escaped backticks', () => {
+          const original = '`This is a \\`test\\`.`';
+          const expected = '"This is a `test`."';
+
+          const actual = replaceQuoteChars(original, '"');
+
+          assert.equal(actual, expected);
+        });
+      });
+    });
+
+    suite('target: backticks', () => {
+      suite('simple strings: the quotes should be replaced', () => {
+        test('from single quotes', () => {
+          const original = `'This is a test.'`;
+          const expected = '`This is a test.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from double quotes', () => {
+          const original = `"This is a test."`;
+          const expected = '`This is a test.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from backticks', () => {
+          const original = '`This is a test.`';
+          const expected = '`This is a test.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including backticks: backticks should be escaped', () => {
+        test('from single quotes', () => {
+          const original = '\'This is a `test`.\'';
+          const expected = '`This is a \\`test\\`.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from double quotes', () => {
+          const original = '"This is a `test`."';
+          const expected = '`This is a \\`test\\`.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+      });
+
+      suite('strings including escaped quotes: should not be escaped afterwards', () => {
+        test('from single quotes, including escaped single quotes', () => {
+          const original = `'This is a \\'test\\'.'`;
+          const expected = '`This is a \'test\'.`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+
+        test('from double quotes, including escaped double quotes', () => {
+          const original = `"This is a \\"test\\"."`;
+          const expected = '`This is a "test".`';
+
+          const actual = replaceQuoteChars(original, '`');
+
+          assert.equal(actual, expected);
+        });
+      });
+    });
+  });
+
+  suite('replaceLineBreaks', () => {
+    test('all line break types should be replaced', () => {
+      const original = 'This\ris\r\na\ntest.';
+      const expected = 'This is a test.';
+
+      const actual = replaceLineBreaks(original);
+
+      assert.equal(actual, expected);
+    });
+
+    test('all line break types should be replaced with the specified string', () => {
+      const original = 'This\ris\r\na\ntest.';
+      const expected = 'This@is@a@test.';
+
+      const actual = replaceLineBreaks(original, '@');
+
+      assert.equal(actual, expected);
+    });
+  });
+
+  suite('wrapText', () => {
+    test('the text should be wrapped with the specified string', () => {
+      const original = 'Inside';
+      const expected = '@@@Inside@@@';
+
+      const actual = wrapText(original, '@@@');
+
+      assert.equal(actual, expected);
+    });
+  });
+});
