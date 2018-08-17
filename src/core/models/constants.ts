57412eac2c63fe817710398cc2c624d29c829079
Alexander Dießl <42088160+adiessl@users.noreply.github.com>
2018-08-17 21:22:31 +0200

    Change enum order

--- src/core/models/constants.ts
+++ src/core/models/constants.ts
@@ -1,11 +1,11 @@
 export enum StringType {
+  TEMPLATE_LITERAL,
   SINGLE_QUOTE,
-  DOUBLE_QUOTE,
-  TEMPLATE_LITERAL
+  DOUBLE_QUOTE
 }
 
 export const StringKindToQuote: { [type in StringType]: string } = {
+  [StringType.TEMPLATE_LITERAL]: '`',
   [StringType.SINGLE_QUOTE]: '\'',
-  [StringType.DOUBLE_QUOTE]: '"',
-  [StringType.TEMPLATE_LITERAL]: '`'
+  [StringType.DOUBLE_QUOTE]: '"'
 };
