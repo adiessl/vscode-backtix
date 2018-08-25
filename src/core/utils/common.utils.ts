8504bb18142fce44c80e1fd4db59f8eb17ff3d53
Alexander Dießl <42088160+adiessl@users.noreply.github.com>
2018-08-25 20:33:47 +0200

    Extract function for converting a node's location to a range

--- src/core/utils/common.utils.ts
+++ src/core/utils/common.utils.ts
@@ -25,15 +25,17 @@ export function retrieveNodes(sourceFile: ts.SourceFile, syntaxKinds: { [kind in
 }
 
 export function convertToDiagnostic(textDocument: vscode.TextDocument, replacement: NodeReplacement, messages: { [t in StringType]: string }): vscode.Diagnostic {
-  const node = replacement.node;
-
-  const start = textDocument.positionAt(node.getStart());
-  const end = textDocument.positionAt(node.getEnd());
-
-  const range = new vscode.Range(start, end);
+  const range = getDocumentRange(textDocument, replacement.node);
 
   const diagnostic = new vscode.Diagnostic(range, messages[replacement.targetType], vscode.DiagnosticSeverity.Hint);
   diagnostic.code = replacement.replacement;
 
   return diagnostic;
+}
+
+function getDocumentRange(textDocument: vscode.TextDocument, node: ts.Node): vscode.Range {
+  const start = textDocument.positionAt(node.getStart());
+  const end = textDocument.positionAt(node.getEnd());
+
+  return new vscode.Range(start, end);
 }
