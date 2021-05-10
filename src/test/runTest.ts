67c56709f94e46ee5ac10060a18da93ea457531c
Alexander Diessl <42088160+adiessl@users.noreply.github.com>
2021-05-10 22:27:37 +0200

    Run tests using stable and insiders version of VS Code

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,8 +2,12 @@ import * as path from 'path';
 
 import { runTests } from 'vscode-test';
 
-async function main() {
+async function main(args: string[]) {
     try {
+        const version = args[0] ?? 'stable';
+
+        console.log(`Running tests using VS Code version "${version}".`);
+
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../');
@@ -13,11 +17,11 @@ async function main() {
         const extensionTestsPath = path.resolve(__dirname, './index');
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        await runTests({ extensionDevelopmentPath, extensionTestsPath, version });
     } catch (err) {
         console.error('Failed to run tests', err);
         process.exit(1);
     }
 }
 
-main();
+main(process.argv.slice(2));
