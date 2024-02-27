8ee407c357d357f22b3c863187d42b869ffbb191
Alexander Diessl <42088160+adiessl@users.noreply.github.com>
2024-02-27 19:04:24 +0100

    Set mocha timeout for tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,13 +3,19 @@ import { defineConfig } from '@vscode/test-cli';
 const stableConfig = defineConfig({
 	files: 'out/test/**/*.test.js',
 	version: 'stable',
-	label: 'stable'
+	label: 'stable',
+	mocha: {
+		timeout: 10000
+	}
 });
 
 const insidersConfig = defineConfig({
 	files: 'out/test/**/*.test.js',
 	version: 'insiders',
-	label: 'insiders'
+	label: 'insiders',
+	mocha: {
+		timeout: 10000
+	}
 });
 
 export default [stableConfig, insidersConfig];
