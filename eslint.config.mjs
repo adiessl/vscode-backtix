ac49e4d88d0668d834560e346c0460826aa88436
Alexander Diessl <42088160+adiessl@users.noreply.github.com>
2025-02-22 14:25:08 +0100

    Migrate to ESLint Stylistic
    https://eslint.org/blog/2023/10/deprecating-formatting-rules/
    https://eslint.style/guide/migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
 import js from "@eslint/js";
 import typescriptESLint from "typescript-eslint";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
     js.configs.recommended,
     ...typescriptESLint.configs.recommended,
     {
         files: ["**/*.ts"],
+        plugins: {
+            '@stylistic': stylistic
+        },
         rules: {
             "@typescript-eslint/naming-convention": [
                 "warn",
@@ -19,7 +23,7 @@ export default [
             curly: "warn",
             eqeqeq: "warn",
             "no-throw-literal": "warn",
-            semi: "off"
+            "@stylistic/semi": "warn"
         },
         ignores: ["**/*.d.ts"]
     }
